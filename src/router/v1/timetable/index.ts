50d136f7b5683a42ef80af9f175bb2f627861376
vriskaserket51 <serketvriska51@gmail.com>
2022-10-26 15:47:41 +0900

    Student page api updated

---
+++ src/router/v1/timetable/index.ts
@@ -0,0 +1,146 @@
+import { Request, Response } from "express";
+import * as v1 from "@common-jshs/menkakusitsu-lib/v1";
+import V1 from "..";
+import { execute, query } from "../../../mysql";
+import { ResponseException, HttpException } from "../../../exceptions";
+import { defaultErrorHandler } from "../../../utils/ErrorHandler";
+import {
+    escapeUserName,
+    getJwtPayload,
+    getStudentInfo,
+    getTeacherInfo,
+    getUserInfo,
+} from "../../../utils/Utility";
+import fs from "fs";
+import path from "path";
+
+class Timetable extends V1 {
+    constructor() {
+        super();
+        this.setPath("/timetable");
+        this.models = [
+            {
+                method: "get",
+                path: "/:when",
+                // authType: "access",
+                controller: Timetable.onGetTimetable,
+            },
+            {
+                method: "put",
+                path: "/:when",
+                // authType: "access",
+                controller: Timetable.onPutTimetable,
+            },
+        ];
+    }
+
+    static async onGetTimetable(req: Request, res: Response) {
+        try {
+            const getTimetableRequest: v1.GetTimetableRequest =
+                req.params as any;
+            if (!getTimetableRequest.when) {
+                throw new HttpException(400);
+            }
+
+            const timetableQuery: v1.TimetableCell[] = (await query(
+                "SELECT `key`, `value` FROM timetable WHERE `when`=?",
+                [getTimetableRequest.when]
+            )) as any;
+
+            const findValue = (key: string) => {
+                for (const timetableInfo of timetableQuery) {
+                    if (timetableInfo.key === key) {
+                        return timetableInfo.value;
+                    }
+                }
+                execute(
+                    "INSERT INTO timetable(`key`, `value`, `when`) VALUE(?, ?, ?)",
+                    [key, "null", getTimetableRequest.when]
+                );
+                return "null";
+            };
+
+            const parseTimetable = (path: string): v1.TimetableCell[][] => {
+                const csv = fs.readFileSync(path, "utf-8");
+                const rows = csv.split("\n");
+                const result: v1.TimetableCell[][] = [];
+                for (let i = 0; i < rows.length; i++) {
+                    const columns = rows[i].split(",");
+                    const cellRow: v1.TimetableCell[] = [];
+                    for (let j = 0; j < columns.length; j++) {
+                        if (columns[j].startsWith("data:")) {
+                            const key = columns[j].split("data:")[1];
+                            cellRow.push({ key: key, value: findValue(key) });
+                        } else {
+                            cellRow.push({ key: "", value: columns[j] });
+                        }
+                    }
+                    result.push(cellRow);
+                }
+                return result;
+            };
+
+            const timetableInfo = parseTimetable(
+                path.join(
+                    __dirname,
+                    "..",
+                    "..",
+                    "..",
+                    "files",
+                    "timetable_normal.csv"
+                )
+            );
+
+            const getTimetableResponse: v1.GetTimetableResponse = {
+                status: 0,
+                message: "",
+                timetable: {
+                    timetableInfo: timetableInfo,
+                },
+            };
+            res.status(200).json(getTimetableResponse);
+        } catch (error) {
+            defaultErrorHandler(res, error);
+        }
+    }
+
+    static async onPutTimetable(req: Request, res: Response) {
+        try {
+            const putTimetableRequest: v1.PutTimetableRequest = req.body;
+            putTimetableRequest.when = req.params.when;
+            if (
+                !putTimetableRequest.when ||
+                !putTimetableRequest.timetableInfo
+            ) {
+                throw new HttpException(400);
+            }
+
+            for (const timetableCell of putTimetableRequest.timetableInfo) {
+                if (!timetableCell.key) {
+                    continue;
+                }
+                await execute(
+                    "UPDATE timetable SET `value`=? WHERE `when`=? AND `key`=?",
+                    [
+                        timetableCell.value,
+                        putTimetableRequest.when,
+                        timetableCell.key,
+                    ]
+                );
+            }
+
+            const putTimetableResponse: v1.PutTimetableResponse = {
+                status: 0,
+                message: "",
+                timetable: {
+                    timetableInfo: [],
+                },
+            };
+            res.status(200).json(putTimetableResponse);
+        } catch (error) {
+            defaultErrorHandler(res, error);
+        }
+    }
+}
+
+export default Timetable;
