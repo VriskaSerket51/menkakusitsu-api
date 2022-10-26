770309d83a8861352a6cc8906fba1985de31761d
vriskaserket51 <serketvriska51@gmail.com>
2022-10-26 22:37:11 +0900

    Add scheduler

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,14 @@ import dotenv from "dotenv";
 import App from "./app";
 import config from "./config";
 import { initializeFirebase } from "./firebase";
+import { initSchedule as initializeScheduler } from "./scheduler";
 import { logger } from "./utils/Logger";
 
 dotenv.config();
 const port = parseInt(config.port);
 
 initializeFirebase();
+initializeScheduler();
 
 const app = new App();
 app.run(
