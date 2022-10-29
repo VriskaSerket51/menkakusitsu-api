1b9be8ecd0032d8dd71e1e5617100fcb1781c40e
vriskaserket51 <serketvriska51@gmail.com>
2022-10-29 21:59:56 +0900

    소유자 ID 체크 삭제

--- src/router/v1/bbs/index.ts
+++ src/router/v1/bbs/index.ts
@@ -227,11 +227,11 @@ class Bbs extends V1 {
                 throw new HttpException(403);
             }
             await execute(
-                "UPDATE bbs_post SET deletedDate=NOW() WHERE ownerUid=? AND id=?",
+                "UPDATE bbs_post SET deletedDate=NOW() WHERE AND id=?",
                 [payload.uid, request.id]
             );
             await execute(
-                "UPDATE bbs_comment SET deletedDate=NOW() WHERE ownerUid=? AND postId=?",
+                "UPDATE bbs_comment SET deletedDate=NOW() WHERE postId=?",
                 [payload.uid, request.id]
             );
             const response: v1.DeleteBbsPostResponse = {
@@ -355,7 +355,7 @@ class Bbs extends V1 {
                 throw new HttpException(403);
             }
             await execute(
-                "UPDATE bbs_comment SET deletedDate=NOW() WHERE ownerUid=? AND id=?",
+                "UPDATE bbs_comment SET deletedDate=NOW() WHERE AND id=?",
                 [payload.uid, request.id]
             );
             const response: v1.DeleteBbsCommentResponse = {
