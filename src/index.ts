6b6e77fdf0a5302a9f6d154e65faa122ca32649c
vriskaserket51 <serketvriska51@gmail.com>
2022-11-03 08:50:58 +0900

    aes iv 추가

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import { initializeFirebase } from "./firebase";
 import { execute, query } from "./mysql";
 import { initializeScheduler } from "./scheduler";
 import { logger } from "./utils/Logger";
-import { aes256Encrypt } from "./utils/Utility";
+import { aes256Decrypt, aes256Encrypt } from "./utils/Utility";
 
 dotenv.config();
 const port = parseInt(config.port);
@@ -31,9 +31,9 @@ function runExpressApp() {
 async function testFunction() {
     const q = await query("SELECT * FROM user", []);
     for (const userInfo of q) {
-        if (userInfo.password) {
-            await execute("UPDATE user SET password=? WHERE UID=?", [
-                aes256Encrypt(userInfo.password),
+        if (userInfo.ID) {
+            await execute("UPDATE user SET ID=? WHERE UID=?", [
+                aes256Encrypt(userInfo.ID),
                 Number(userInfo.UID),
             ]);
         }
