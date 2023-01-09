3b90486abf4fc08133df1c84ea8ac49723407627
vriskaserket51 <serketvriska51@gmail.com>
2023-01-10 00:43:46 +0900

    소켓 제거

--- src/app.ts
+++ src/app.ts
@@ -1,29 +1,20 @@
+require("express-async-errors");
 import express from "express";
-import http from "http";
 import cors from "cors";
 import helmet from "helmet";
 import { defaultRouter } from "./router";
 import fileUpload from "express-fileupload";
 import path from "path";
-import { Server } from "socket.io";
-import { DefaultEventsMap } from "socket.io/dist/typed-events";
+import { errorHandler } from "./middlewares";
 
 class App {
     expressApp: express.Application;
-    server: http.Server<
-        typeof http.IncomingMessage,
-        typeof http.ServerResponse
-    >;
-    io: Server<DefaultEventsMap, DefaultEventsMap, DefaultEventsMap, any>;
 
     constructor() {
         this.expressApp = express();
-        this.server = http.createServer(this.expressApp);
-        this.io = new Server(this.server);
         this.initMiddlewares();
         this.initRouters();
-        this.initSocket();
-        this.initLateMiddlewares();
+        this.initErrorHandlers();
     }
 
     run(
@@ -31,7 +22,7 @@ class App {
         onSuccessed: () => void,
         onFailed: (...args: any[]) => void
     ) {
-        this.server.listen(port, onSuccessed).on("error", onFailed);
+        this.expressApp.listen(port, onSuccessed).on("error", onFailed);
     }
 
     initMiddlewares() {
@@ -51,14 +42,8 @@ class App {
         this.expressApp.use("/", defaultRouter);
     }
 
-    initSocket() {
-        this.io.use
-    }
-
-    initLateMiddlewares() {
-        this.expressApp.use((req, res, next) => {
-            res.sendStatus(404);
-        });
+    initErrorHandlers() {
+        this.expressApp.use(errorHandler);
     }
 }
 
