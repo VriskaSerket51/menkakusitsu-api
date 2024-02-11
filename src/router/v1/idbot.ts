60a0b9c3a7aa12ebc70b26c466e6d9f7869bb298
vriskaserket51 <serketvriska51@gmail.com>
2024-02-11 15:32:12 +0900

    handle idbot error

--- src/router/v1/idbot.ts
+++ src/router/v1/idbot.ts
@@ -26,10 +26,27 @@ class Idbot extends Chat {
       throw new CommonApi.HttpException(400);
     }
 
-    const resp = await fetch(
-      `http://127.0.0.1:3001/idbot/message?chatInput=${request.chatInput}`
-    );
-    const chatOutput = ((await resp.json()) as any).chatOutput;
+    let chatOutput: string;
+    try {
+      const resp = await fetch(
+        `http://127.0.0.1:3001/idbot/message?chatInput=${request.chatInput}`
+      );
+      if (resp.status != 200) {
+        throw new CommonApi.ResponseException(
+          -1,
+          "이디봇에 에러가 발생하였습니다."
+        );
+      }
+
+      chatOutput = ((await resp.json()) as any).chatOutput;
+    } catch (error) {
+      CommonApi.logger.error(error);
+      throw new CommonApi.ResponseException(
+        -2,
+        "이디봇이 작동하고 있지 않습니다."
+      );
+    }
+
     const response: v1.GetIdbotChatResponse = {
       status: 0,
       message: "",
